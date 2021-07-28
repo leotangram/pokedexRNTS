7aa7459a239d5661429b4555dc1383e7bf65a0c6
Leonardo Omaña <leotangram@gmail.com>
2021-07-28 15:27:34 -0500

    Add data type

--- src/hooks/usePokemonPaginated.ts
+++ src/hooks/usePokemonPaginated.ts
@@ -1,17 +1,35 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { pokemonApi } from '../api/pokemonApi'
+import { Result } from '../interfaces/pokemonInterfaces'
+import {
+  PokemonPaginatedResponse,
+  SimplePokemon
+} from '../interfaces/pokemonInterfaces'
 
 export const usePokemonPaginated = () => {
-  const nextPageUrl = useRef(
-    'https://pokeapi.co/api/v2/pokemon?limit=40'
-  ).current
+  const nextPageUrl = useRef('https://pokeapi.co/api/v2/pokemon?limit=40')
+
+  const [simplePokemonList, setSimplePokemonList] = useState<SimplePokemon[]>(
+    []
+  )
 
   const loadPokemons = async () => {
-    const response = await pokemonApi.get(nextPageUrl)
-    console.log(response.data)
+    const response = await pokemonApi.get<PokemonPaginatedResponse>(
+      nextPageUrl.current
+    )
+    nextPageUrl.current = response.data.next
+    mapPokemonsToSimplePokemons(response.data.results)
+  }
+
+  const mapPokemonsToSimplePokemons = (pokemons: Result[]) => {
+    pokemons.forEach(pokemon => console.log(pokemon.name))
   }
 
   useEffect(() => {
     loadPokemons()
   }, [])
+
+  return {
+    simplePokemonList
+  }
 }
